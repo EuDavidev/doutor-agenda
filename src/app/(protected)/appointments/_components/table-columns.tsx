2024f87be6699c9e112cd1dc5596bc54be0fcd87
Davvwi <davisouza128@gmail.com>
2025-06-11 13:25:43 -0300

    refactor: mover definição do tipo AppointmentWithRelations para um novo arquivo e corrigir ortografia de 'specialty' para 'speciality'

--- src/app/(protected)/appointments/_components/table-columns.tsx
+++ src/app/(protected)/appointments/_components/table-columns.tsx
@@ -4,24 +4,8 @@ import { ColumnDef } from "@tanstack/react-table";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
-import { appointmentsTable } from "@/db/schema";
-
 import AppointmentsTableActions from "./table-actions";
-
-type AppointmentWithRelations = typeof appointmentsTable.$inferSelect & {
-  patient: {
-    id: string;
-    name: string;
-    email: string;
-    phoneNumber: string;
-    sex: "male" | "female";
-  };
-  doctor: {
-    id: string;
-    name: string;
-    specialty: string;
-  };
-};
+import { AppointmentWithRelations } from "./types";
 
 export const appointmentsTableColumns: ColumnDef<AppointmentWithRelations>[] = [
   {
---
+++ src/app/(protected)/appointments/_components/types.ts
@@ -0,0 +1,16 @@
+import { appointmentsTable } from "@/db/schema";
+
+export type AppointmentWithRelations = typeof appointmentsTable.$inferSelect & {
+  patient: {
+    id: string;
+    name: string;
+    email: string;
+    phoneNumber: string;
+    sex: "male" | "female";
+  };
+  doctor: {
+    id: string;
+    name: string;
+    speciality: string;
+  };
+};
